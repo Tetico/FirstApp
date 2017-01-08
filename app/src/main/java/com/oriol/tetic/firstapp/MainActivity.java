64592e6e5b4ea3bb8e3e7fb278ca924550c67e91
Tetico <oriolrosmartin@gmail.com>
2017-01-08 22:46:05 +0100

    Initial commit

---
+++ app/src/main/java/com/oriol/tetic/firstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.oriol.tetic.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
